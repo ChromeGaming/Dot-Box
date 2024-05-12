9b6581254721da824775f59e9499aa549f0f5b18
Ufraan <ufraansiddiqui30@gmail.com>
2024-05-12 14:21:03 +0530

    optimizations for game script

--- js/game.js
+++ js/game.js
@@ -1,19 +1,19 @@
 class Game {
-	static instance //Singleton instance of Game class
+	static instance; //singleton instance of game class
 
 	constructor(rows, columns, playersCount) {
-		if (Game.instance == null) Game.instance = this
+		if (Game.instance == null) Game.instance = this;
 
-		this.playersUI = document.querySelector(".players")
-		this.playerNameUI = document.querySelector(".player-turn .name")
-		this.playerTurnBgUI = document.querySelector(".player-turn .bg")
+		this.playersUI = document.querySelector(".players");
+		this.playerNameUI = document.querySelector(".player-turn .name");
+		this.playerTurnBgUI = document.querySelector(".player-turn .bg");
 
 		this.events = {
 			edgeFill: [],
 			boxFill: [],
 			playerSwitch: [],
 			playerWin: [],
-		}
+		};
 
 		this.players = [
 			{ name: "Player 1", color: "pink", filledBoxes: 0 },
@@ -21,53 +21,56 @@ class Game {
 			{ name: "Player 3", color: "lightgreen", filledBoxes: 0 },
 			{ name: "Player 4", color: "magenta", filledBoxes: 0 },
 			{ name: "Player 5", color: "yellow", filledBoxes: 0 },
-			{ name: "Player 6", color: "orange", filledBoxes: 0 }
-		]
+			{ name: "Player 6", color: "orange", filledBoxes: 0 },
+		];
 
-		let p = this.players.length - playersCount
-		for (let i = 0; i < p; i++)
-			this.players.pop()
+		let p = this.players.length - playersCount;
+		for (let i = 0; i < p; i++) this.players.pop();
 
-		this.currentPlayerIndex = 0
-		this.currentPlayer = this.players[this.currentPlayerIndex]
+		this.currentPlayerIndex = 0;
+		this.currentPlayer = this.players[this.currentPlayerIndex];
 
-		this.board = new Board(rows, columns)
+		this.board = new Board(rows, columns);
 
-		this.isGameover = false
+		this.isGameover = false;
 
-		this.addPlayersUI()
-		this.updatePlayerNameUI()
+		this.addPlayersUI();
+		this.updatePlayerNameUI();
+		this.updatePlayerScoreUI = this.updatePlayerScoreUI.bind(this);
+		this.onBoxFill = this.onBoxFill.bind(this);
+		this.onPlayerSwitch = this.onPlayerSwitch.bind(this);
+		this.onPlayerWin = this.onPlayerWin.bind(this);
 
-		//Adding event listeners for filling box, switching player and winning
-		this.addEventListener("boxFill", () => this.onBoxFill())
-		this.addEventListener("playerSwitch", () => this.onPlayerSwitch())
-		this.addEventListener("playerWin", () => this.onPlayerWin())
+		//adding event listeners for filling box, switching player and winning
+		this.addEventListener("boxFill", this.onBoxFill);
+		this.addEventListener("playerSwitch", this.onPlayerSwitch);
+		this.addEventListener("playerWin", this.onPlayerWin);
 	}
 
-	//End Game
+	//end Game
 	onPlayerWin() {
-		this.isGameover = true
+		this.isGameover = true;
 
 		bgMusic.pause();
-		let winSound = new Audio('./sounds/win.mp3');
+		let winSound = new Audio("./sounds/win.mp3");
 		winSound.play();
-		
-		const player = this.players.reduce((prev, current) => {
-			return prev.filledBoxes > current.filledBoxes ? prev : current
-		});
+
+		const player = this.players.reduce((prev, current) =>
+			prev.filledBoxes > current.filledBoxes ? prev : current
+		);
 
 		setTimeout(() => {
-			let play = this.players[0].filledBoxes
+			let play = this.players[0].filledBoxes;
 
 			//Check for winner
 			if (this.players.every((p) => p.filledBoxes == play)) {
-				this.playerNameUI.parentElement.textContent = "Nobody wins"
-				this.playerTurnBgUI.classList.add("no-win")
-				this.playerTurnBgUI.style.background = "#eaeaea"
+				this.playerNameUI.parentElement.textContent = "Nobody wins";
+				this.playerTurnBgUI.classList.add("no-win");
+				this.playerTurnBgUI.style.background = "#eaeaea";
 			} else {
-				this.playerNameUI.parentElement.textContent = `${player.name} wins`
-				this.playerTurnBgUI.classList.add("win")
-				this.playerTurnBgUI.style.background = player.color
+				this.playerNameUI.parentElement.textContent = `${player.name} wins`;
+				this.playerTurnBgUI.classList.add("win");
+				this.playerTurnBgUI.style.background = player.color;
 			}
 		}, 500);
 	}
@@ -76,114 +79,112 @@ class Game {
 		this.updatePlayerNameUI();
 	}
 
-	//If a box if filled, increament players score with number of boxes filled by him/her and update UI
+	//if a box if filled, increment player's score with number of boxes filled by him/her and update UI
 	onBoxFill() {
-		this.currentPlayer.filledBoxes++
-		this.updatePlayerScoreUI();
+		this.currentPlayer.filledBoxes++;
+		requestAnimationFrame(this.updatePlayerScoreUI);
 	}
 
-	//Add players to UI
+	//add players to UI
 	addPlayersUI() {
 		this.players.forEach((player, index) => {
-			const div = document.createElement("div")
-			div.classList.add("player")
+			const div = document.createElement("div");
+			div.classList.add("player");
 
-			//Maintain filled boxes.
-			const b = document.createElement("b")
-			b.classList.add("filled-boxes")
-			b.textContent = player.filledBoxes
-			b.style.background = player.color
-			this.players[index]["filledBoxesUI"] = b
+			//maintain filled boxes.
+			const b = document.createElement("b");
+			b.classList.add("filled-boxes");
+			b.textContent = player.filledBoxes;
+			b.style.background = player.color;
+			this.players[index]["filledBoxesUI"] = b;
 
-			//Maintain player name.
-			const span = document.createElement("span")
-			span.textContent = player.name
+			//maintain player name.
+			const span = document.createElement("span");
+			span.textContent = player.name;
 
-			div.appendChild(b)
-			div.appendChild(span)
+			div.appendChild(b);
+			div.appendChild(span);
 
-			//Adding score and name to the element
-			this.playersUI.appendChild(div)
+			//adding score and name to the element
+			this.playersUI.appendChild(div);
 		});
 	}
 
-	//Update player score UI used while switching player
+	//update player score UI used while switching player
 	updatePlayerScoreUI() {
-		this.currentPlayer.filledBoxesUI.innerText = this.currentPlayer.filledBoxes
+		this.currentPlayer.filledBoxesUI.innerText = this.currentPlayer.filledBoxes;
 	}
 
-	//Update player name UI used while switching player
+	//update player name UI used while switching player
 	updatePlayerNameUI() {
-		this.playerNameUI.innerText = this.currentPlayer.name
-		this.playerTurnBgUI.style.background = this.currentPlayer.color
+		this.playerNameUI.innerText = this.currentPlayer.name;
+		this.playerTurnBgUI.style.background = this.currentPlayer.color;
 	}
 
 	eventExist(event) {
-		return this.events.hasOwnProperty(event)
+		return this.events.hasOwnProperty(event);
 	}
 
-	//Add event listeners
+	//add event listeners
 	addEventListener(event, callback) {
 		if (!this.eventExist(event)) {
-			console.error(`${event} event is not defined`)
-			return
+			console.error(`${event} event is not defined`);
+			return;
 		}
 
-		this.events[event].push(callback)
+		this.events[event].push(callback);
 	}
 
-	//Remove event listeners
+	//remove event listeners
 	removeEventListener(event, callback) {
 		if (!this.eventExist(event)) {
-			console.error(`${event} event is not defined`)
-			return
+			console.error(`${event} event is not defined`);
+			return;
 		}
-		this.events[event].splice(this.events[event].indexOf(callback), 1)
+		this.events[event].splice(this.events[event].indexOf(callback), 1);
 	}
 
-	//Invoke event listeners
+	//invoke event listeners
 	invokeEvent(event, args) {
 		if (!this.eventExist(event)) {
-			console.error(`${event} event is not defined`)
-			return
+			console.error(`${event} event is not defined`);
+			return;
 		}
-		this.events[event].forEach((callback) => callback(args))
+		this.events[event].forEach((callback) => callback(args));
 	}
 
-	//Switch player
+	//switch player
 	switchPlayer() {
 		if (!this.isGameover) {
-			this.currentPlayerIndex = ++this.currentPlayerIndex % this.players.length
-			this.currentPlayer = this.players[this.currentPlayerIndex]
-			this.invokeEvent("playerSwitch")
+			this.currentPlayerIndex = ++this.currentPlayerIndex % this.players.length;
+			this.currentPlayer = this.players[this.currentPlayerIndex];
+			this.invokeEvent("playerSwitch");
 		}
 	}
 }
 
-// Declaring Global Variables
-
-const settingsUI = document.querySelector(".settings")
-const rowsInput = document.querySelector("#rows")
-const columnsInput = document.querySelector("#columns")
-const playersInput = document.querySelector("#players-count")
-const startBtn = document.querySelector(".start-btn")
-const heading = document.querySelector(".heading")
-const bgMusic = new Audio('./sounds/bgMusic.mp3');
-var game = null
+// declaring global variables
+const settingsUI = document.querySelector(".settings");
+const rowsInput = document.querySelector("#rows");
+const columnsInput = document.querySelector("#columns");
+const playersInput = document.querySelector("#players-count");
+const startBtn = document.querySelector(".start-btn");
+const heading = document.querySelector(".heading");
+const bgMusic = new Audio("./sounds/bgMusic.mp3");
+let game = null;
 
 startBtn.addEventListener("click", () => {
 	bgMusic.volume = 0.1;
 	bgMusic.play();
-	const rows = calculate(rowsInput.value, 5, 30)
-	const columns = calculate(columnsInput.value, 5, 30)
-	const playersCount = calculate(playersInput.value, 2, 6)
+	const rows = calculate(rowsInput.value, 5, 30);
+	const columns = calculate(columnsInput.value, 5, 30);
+	const playersCount = calculate(playersInput.value, 2, 6);
 
-
-	game = new Game(rows, columns, playersCount)
-	settingsUI.style.display = "none"
-	heading.style.display = "none"
+	game = new Game(rows, columns, playersCount);
+	settingsUI.style.display = "none";
+	heading.style.display = "none";
 });
 
 function calculate(value, min, max) {
-	return Math.min(Math.max(value, min), max)
+	return Math.min(Math.max(value, min), max);
 }
