5dc7fac8c2c529299becc76d6303a2d7ccd61b98
Aditya Bhaumik <92214013+aditya-bhaumik@users.noreply.github.com>
2024-05-18 01:41:53 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -38,3 +38,33 @@ document.getElementById('theme-button').addEventListener('click', () => {
     }
 });
 
+const backgroundVideo = document.getElementById('background-video');
+const toggleButton = document.getElementById('toggle-background');
+const backgroundVideos = [
+    './background/back3.mp4',
+    './background/back4.mp4',
+    './background/back5.mp4'
+];
+let currentIndex = 1; // Start from index 1, which is back4
+
+// Function to toggle background video
+function toggleBackground() {
+    currentIndex = (currentIndex + 1) % backgroundVideos.length;
+    backgroundVideo.src = backgroundVideos[currentIndex];
+    backgroundVideo.play();
+}
+
+// Add event listener to toggle background button
+toggleButton.addEventListener('click', toggleBackground);
+
+
+// Add event listener for theme button to toggle visibility of theme options
+document.getElementById('theme-button').addEventListener('click', () => {
+    const themeOptions = document.getElementById('theme-options');
+    if (themeOptions.style.visibility === 'hidden' || themeOptions.style.visibility === '') {
+        themeOptions.style.visibility = 'visible';
+    } else {
+        themeOptions.style.visibility = 'hidden';
+    }
+});
+
