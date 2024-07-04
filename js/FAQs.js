3808b81e01ede0c3fa75b0c2acb61ea14d59ecd0
partha120804 <138643473+partha120804@users.noreply.github.com>
2024-07-04 19:10:49 +0530

    fixed

--- js/FAQs.js
+++ js/FAQs.js
@@ -1,62 +1,47 @@
-const img = document.querySelector(".images");
-const drop = document.querySelector(".drop");
-
-const img2 = document.querySelector(".images2");
-const drop2 = document.querySelector(".drop2");
-
-const img3 = document.querySelector(".images3");
-const drop3 = document.querySelector(".drop3");
-
-const img4 = document.querySelector(".images4");
-const drop4 = document.querySelector(".drop4");
-
-let toggle = true;
-
-
-drop.addEventListener('click', function(){
-    toggle= !toggle;
-    if(toggle)
-    {
+function toggle(drop, isExpanded) {
+    const img = drop.querySelector('img');
+    if (isExpanded) {
+        img.src="../assets/images/icon-minus.svg";
+    }else {
         img.src="../assets/images/icon-plus.svg";
     }
-    else
-    {
-        img.src="../assets/images/icon-minus.svg"
-    }
-})
+}
+
+//Drops and their default expansion values
+const drop1 =   document.querySelector(".drop1")  ;     let drop1isExpanded = false   ;
+const drop2 =   document.querySelector(".drop2")  ;     let drop2isExpanded = false   ;
+const drop3 =   document.querySelector(".drop3")  ;     let drop3isExpanded = false   ;
+const drop4 =   document.querySelector(".drop4")  ;     let drop4isExpanded = false   ;
+const drop5 =   document.querySelector(".drop5")  ;     let drop5isExpanded = false   ;
+const drop6 =   document.querySelector(".drop6")  ;     let drop6isExpanded = false   ;
+
+
+drop1.addEventListener('click', function(){
+drop1isExpanded = !drop1isExpanded;
+toggle(drop1, drop1isExpanded);
+});
 
 drop2.addEventListener('click', function(){
-    toggle= !toggle;
-    if(toggle)
-    {
-        img2.src="../assets/images/icon-plus.svg";
-    }
-    else
-    {
-        img2.src="../assets/images/icon-minus.svg"
-    }
-})
+drop2isExpanded = !drop2isExpanded;
+toggle(drop2, drop2isExpanded);
+});
 
 drop3.addEventListener('click', function(){
-    toggle= !toggle;
-    if(toggle)
-    {
-        img3.src="../assets/images/icon-plus.svg";
-    }
-    else
-    {
-        img3.src="../assets/images/icon-minus.svg"
-    }
-})
+drop3isExpanded = !drop3isExpanded;
+toggle(drop3, drop3isExpanded);
+});
 
 drop4.addEventListener('click', function(){
-    toggle= !toggle;
-    if(toggle)
-    {
-        img4.src="../assets/images/icon-plus.svg";
-    }
-    else
-    {
-        img4.src="../assets/images/icon-minus.svg"
-    }
-})
+drop4isExpanded = !drop4isExpanded;
+toggle(drop4, drop4isExpanded);
+});
+
+drop5.addEventListener('click', function(){
+drop5isExpanded = !drop5isExpanded;
+toggle(drop5, drop5isExpanded);
+});
+
+drop6.addEventListener('click', function(){
+drop6isExpanded = !drop6isExpanded;
+toggle(drop6, drop6isExpanded);
+});
